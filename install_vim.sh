fc5cb8ebbc72dd14b42d8f896d37a0cf532f8cbc
shimamu <cooklecurry@gmail.com>
2025-05-19 22:43:58 +0900

    chore: Switch from vim-dotfiles.git to vim-config.git

--- install_vim.sh
+++ install_vim.sh
@@ -10,17 +10,8 @@ ensure_check_update_done
 log "Installing vim via dnf"
 sudo dnf install -y vim | tee -a "$LOG_FILE"
 
-# Clone Vundle plugin manager
-VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
-if [ ! -d "$VUNDLE_DIR" ]; then
-    log "Cloning Vundle to $VUNDLE_DIR"
-    git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
-else
-    log "Vundle already exists at: $VUNDLE_DIR"
-fi
-
 # Clone vim configuration repo
-CONF_REPO_URL="https://github.com/shimamu/vim-dotfiles.git"
+CONF_REPO_URL="https://github.com/shimamu/vim-config.git"
 CONF_REPO_NAME=$(basename "${CONF_REPO_URL%.git}")
 CONF_PATH="$CONF_DIR/$CONF_REPO_NAME"
 
@@ -33,23 +24,6 @@ fi
 
 log "Running setup.sh in $CONF_PATH"
 cd "$CONF_PATH" || exit 1
-chmod +x setup.sh
-./setup.sh | tee -a "$LOG_FILE"
-
-# Clone vim colors repo
-COLORS_REPO_URL="https://github.com/shimamu/vim-colors.git"
-COLORS_REPO_NAME=$(basename "${COLORS_REPO_URL%.git}")
-COLORS_PATH="$CONF_DIR/$COLORS_REPO_NAME"
-
-if [ ! -d "$COLORS_PATH" ]; then
-    log "Cloning vim colors to $COLORS_PATH"
-    git clone "$COLORS_REPO_URL" "$COLORS_PATH"
-else
-    log "Colors directory already exists: $COLORS_PATH"
-fi
-
-log "Running setup.sh in $COLORS_PATH"
-cd "$COLORS_PATH" || exit 1
 chmod +x setup.sh
 ./setup.sh | tee -a "$LOG_FILE"
 
