fcd085519f10f36a2c1647ba72f9cab3029c28fd
shimamu <cooklecurry@gmail.com>
2025-06-02 00:29:16 +0900

    feat: Add configuration steps for fcitx5

--- install_fcitx5.sh
+++ install_fcitx5.sh
@@ -21,5 +21,15 @@ firefox "https://extensions.gnome.org/extension/261/kimpanel/" &
 
 read -p "After completing the Input Method Panel extension installation in the browser, press [Enter] to continue..."
 
+# Clone fcitx5 config and run setup
+REPO_URL="https://github.com/shimamu/fcitx5-config"
+TARGET_DIR="$CONF_DIR/fcitx5-config"
+
+log "Cloning fcitx5-config repository: $REPO_URL"
+git clone "$REPO_URL" "$TARGET_DIR" | tee -a "$LOG_FILE"
+
+log "Running setup.sh in $TARGET_DIR"
+(cd "$TARGET_DIR" && bash ./setup.sh | tee -a "$LOG_FILE")
+
 log "fcitx5 installation and configuration completed."
 
